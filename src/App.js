08f75be8b30690316fb6cce6962abf46c6c1d0e8
Nishchal <nischal.15gupta@gmail.com>
2024-01-10 20:26:18 +0530

    boilerplate react setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+    <h1>Hello Nishchal 
+    Congratulate yourself that you are on the path of becoming a high value man
+    </h1>
+    </div>
+  );
+}
+
+export default App;
