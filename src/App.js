66f05bccf5dfa60852c56226fbc8c9bedfa3b18c
Nishchal <nischal.15gupta@gmail.com>
2024-01-15 17:46:35 +0530

    Registration UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from "react";
+import Register from "./components/Register/Register";
+
 function App() {
   return (
     <div>
-    <h1>Hello Nishchal 
-    Congratulate yourself that you are on the path of becoming a high value man
-    </h1>
+    <Register/>
     </div>
   );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,74 @@
 import React from 'react'
+import styles from './Register.module.css';
+import music from '../Assets/music.png'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+      <div className={styles.leftSection}>
+        <img src={music} alt="musicImage" />
+        <h1>Discover new things on Superapp</h1>
+      </div>
+
+      <div className={styles.rightSection}>
+
+        <div className={styles.logo}>
+          <h1>Super app</h1>
+        </div>
+
+        <div className={styles.someText}>
+          <p>Create your new account</p>
+        </div>
+
+        <div className={styles.formData}>
+          <form action="">
+            <div>
+              <label htmlFor="name"></label>
+              <input
+                type="text"
+                name='myName'
+                id='name'
+                placeholder='Name'
+                // style={{ width: '70%', height: '44px' }}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="userName"></label>
+              <input
+                type="text"
+                name='myUserName'
+                id='userName'
+                placeholder='UserName'
+                // style={{ width: '70%', height: '44px' }}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email"></label>
+              <input
+                type="email"
+                name='myEmail'
+                id='email'
+                placeholder='Email'
+                // style={{ width: '70%', height: '44px' }}
+
+              />
+            </div>
+
+            <div>
+              <label htmlFor="mobile"></label>
+              <input
+                type="text"
+                name='myEmail'
+                id='mobile'
+                placeholder='Mobile'
+                // style={{ width: '70%', height: '44px' }}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
