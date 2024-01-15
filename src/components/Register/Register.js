86ad778d894fe53944f00f5c599b1c74b08575ec
Nishchal <nischal.15gupta@gmail.com>
2024-01-15 20:10:41 +0530

    Registration UI Done

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,50 +23,73 @@ export default function Register() {
         <div className={styles.formData}>
           <form action="">
             <div>
-              <label htmlFor="name"></label>
+              <label htmlFor="name">
               <input
                 type="text"
                 name='myName'
                 id='name'
                 placeholder='Name'
-                // style={{ width: '70%', height: '44px' }}
               />
+              </label>
             </div>
 
             <div>
-              <label htmlFor="userName"></label>
+              <label htmlFor="userName">
               <input
                 type="text"
                 name='myUserName'
                 id='userName'
                 placeholder='UserName'
-                // style={{ width: '70%', height: '44px' }}
               />
+              </label>
             </div>
 
             <div>
-              <label htmlFor="email"></label>
+              <label htmlFor="email">
               <input
                 type="email"
                 name='myEmail'
                 id='email'
                 placeholder='Email'
-                // style={{ width: '70%', height: '44px' }}
-
               />
+              </label>
             </div>
 
             <div>
-              <label htmlFor="mobile"></label>
+              <label htmlFor="mobile">
               <input
                 type="text"
                 name='myEmail'
                 id='mobile'
                 placeholder='Mobile'
-                // style={{ width: '70%', height: '44px' }}
               />
+              </label>
+            </div>
+
+            <div className={styles.checkboxClass}>
+              <label htmlFor="myCheckbox">
+                <input
+                  className={styles.checkboxDesign}
+                  type="checkbox"
+                  name="myCheckbox"
+                  id="myCheckbox"
+                />
+              </label>
+              <div><p>Share my registration data with Superapp</p></div>
             </div>
           </form>
+        </div>
+        <div className={styles.buttonClass}>
+          <button><h1>SIGN UP</h1></button>
+        </div>
+        <div className={styles.tAndC}>
+        <p id='one'>By clicking on Sign up. you agree to Superapp&nbsp;
+         <span style={{color:'#72DB73'}}>Terms and Conditions of Use</span> </p>
+        </div>
+        <div className={styles.privacyPolicy}>
+        <p>To learn more about how Superapp collects, uses, 
+        shares and protects your personal data please head Superapp &nbsp;
+        <span style={{color:'#72DB73'}}>Privacy Policy</span></p> 
         </div>
       </div>
     </div>
